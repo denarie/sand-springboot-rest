b3b293e56e7a5099124617565867d1105bf8de9b
Sylvie Denarie <sylvie.denarie@denarie.de>
2025-01-06 18:17:20 +0100

    neue Abfragen für Sand pro Erdteil und Land

--- src/main/java/de/denarie/sand/repositories/CountryRepository.java
+++ src/main/java/de/denarie/sand/repositories/CountryRepository.java
@@ -3,14 +3,24 @@ package de.denarie.sand.repositories;
 import de.denarie.sand.domain.Continent;
 import de.denarie.sand.domain.Country;
 import de.denarie.sand.domain.Person;
+import de.denarie.sand.domain.Sand;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "country", path = "country")
 public interface CountryRepository extends PagingAndSortingRepository<Country, Long>, CrudRepository<Country,Long> {
     Page<Country> findByNameDeLikeIgnoreCase(@Param("name") String name, Pageable pageable);
+
+    @Query(value = "SELECT distinct(s.country) FROM Sand s")
+    Page<Country> findAllCountriesWithSands(Pageable pageable);
+
+    @Query(value = "select co from Country co join co.continents ci where ci.id = :continentId")
+    Page<Country> findByContinentIdWithPagination(@Param("continentId") Integer continentId, Pageable pageable);
 }
--- src/main/java/de/denarie/sand/repositories/SandRepository.java
+++ src/main/java/de/denarie/sand/repositories/SandRepository.java
@@ -9,12 +9,18 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 
 @RepositoryRestResource(collectionResourceRel = "sand", path = "sand")
 public interface SandRepository extends PagingAndSortingRepository<Sand, Long>, CrudRepository<Sand,Long> {
     Page<Sand> findByLongnameLikeIgnoreCase(@Param("name") String name, Pageable pageable);
     Page<Sand> findByCountryId(@Param("countryId") Integer countryId, Pageable pageable);
     Page<Sand> findByPersonsId(@Param("personId") Integer personId, Pageable pageable);
+
     @Query(value = "SELECT s FROM Sand s join s.country co join co.continents ci where ci.id = :continentId")
-    Page<Sand> findAllSandByContintentWithPagination(@Param("continentId") Integer continentId, Pageable pageable);
+    Page<Sand> findAllSandsByContinentWithPagination(@Param("continentId") Integer continentId, Pageable pageable);
+
+    @Query(value = "SELECT s FROM Sand s join s.country co join co.continents ci where ci.id = :continentId")
+    List<Sand> findAllSandsByContinent(@Param("continentId") Integer continentId);
 }
